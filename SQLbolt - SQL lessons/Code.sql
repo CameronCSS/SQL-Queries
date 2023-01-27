4b9bd6dbbcc23cd82a19e7a53c753244f992db70
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-01-27 16:20:34 -0700

    Update Code.sql

--- SQLbolt - SQL lessons/Code.sql
+++ SQLbolt - SQL lessons/Code.sql
@@ -45,4 +45,45 @@ WHERE director <> 'John Lasseter';
 SELECT * FROM movies
 WHERE title LIKE '%Wall-%';
 
+-- List all directors of Pixar movies (alphabetically), without duplicates
+SELECT DISTINCT(director) FROM movies
+ORDER BY director;
+
+-- List the last four Pixar movies released (ordered from most recent to least)
+SELECT title FROM movies
+ORDER BY year DESC LIMIT 4;
+
+-- List the first five Pixar movies sorted alphabetically
+SELECT title FROM movies
+ORDER BY title ASC LIMIT 5;
+
+-- List the first five Pixar movies sorted alphabetically
+SELECT title FROM movies
+ORDER BY title ASC LIMIT 5
+OFFSET 5;
+
+-- List all the Canadian cities and their populations
+SELECT city, population FROM north_american_cities
+WHERE country = 'Canada';
+
+-- Order all the cities in the United States by their latitude from north to south
+SELECT city FROM north_american_cities
+WHERE country = 'United States'
+ORDER BY latitude DESC;
+
+-- List all the cities west of Chicago, ordered from west to east
+SELECT city FROM north_american_cities
+WHERE Longitude < (SELECT Longitude FROM north_american_cities WHERE City = 'Chicago')
+ORDER BY Longitude ASC;
+
+-- List the two largest cities in Mexico (by population)
+SELECT city FROM north_american_cities
+WHERE country = 'Mexico'
+ORDER BY population DESC LIMIT 2;
+
+-- List the third and fourth largest cities (by population) in the United States and their population
+SELECT city FROM north_american_cities
+WHERE country = 'United States'
+ORDER BY population DESC LIMIT 2
+OFFSET 2;
 
