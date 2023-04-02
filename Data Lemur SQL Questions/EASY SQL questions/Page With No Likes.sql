602480f773c0011e50256ae409ceda9f454bb43d
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-04-02 17:41:20 -0600

    Create Page With No Likes.sql

---
+++ Data Lemur SQL Questions/EASY SQL questions/Page With No Likes.sql
@@ -0,0 +1,30 @@
+-- Assume you are given the tables below about Facebook pages and page likes. 
+-- Write a query to return the page IDs of all the Facebook pages that don't have any likes. The output should be in ascending order.
+
+-- pages Table: --
+
+| Column Name |     Type    |
+|   page_id   |   integer   |
+|   page_name |   varchar   |
+
+
+-- page_likes Table: --
+
+|   Column Name |      Type      |
+|   user_id     |    integer     |
+|   page_id     |    integer     |
+|   liked_date  |    datetime    |
+
+
+-- Submission: --
+select pages.page_id
+FROM pages
+LEFT JOIN page_likes
+ON pages.page_id = page_likes.page_id
+GROUP BY pages.page_id
+HAVING COUNT(liked_date) = 0
+
+-- Output: --
+|   page_id |
+|   20701   |
+|   32728   |
