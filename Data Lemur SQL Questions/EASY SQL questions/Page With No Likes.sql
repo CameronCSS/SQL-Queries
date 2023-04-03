a01b06da8cb6bce3c3f2b1243b1a2be59811018b
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-04-02 18:03:11 -0600

    Update Page With No Likes.sql

--- Data Lemur SQL Questions/EASY SQL questions/Page With No Likes.sql
+++ Data Lemur SQL Questions/EASY SQL questions/Page With No Likes.sql
@@ -1,22 +1,16 @@
 -- Assume you are given the tables below about Facebook pages and page likes. 
 -- Write a query to return the page IDs of all the Facebook pages that don't have any likes. The output should be in ascending order.
 
--- pages Table: --
+-- pages Table: --                    -- page_likes Table: --
 
-| Column Name |     Type    |
-|   page_id   |   integer   |
-|   page_name |   varchar   |
+| Column Name | Type    |            | Column Name | Type     |
+|-------------|---------|            |-------------|----------|
+| user_id     | integer |            | page_id     | integer  |
+| page_id     | integer |            | page_name   | varchar  |
+|             |         |            | liked_date  | datetime |
+       
 
-
--- page_likes Table: --
-
-|   Column Name |      Type      |
-|   user_id     |    integer     |
-|   page_id     |    integer     |
-|   liked_date  |    datetime    |
-
-
--- Submission: --
+-- Solution: --
 select pages.page_id
 FROM pages
 LEFT JOIN page_likes
@@ -25,6 +19,10 @@ GROUP BY pages.page_id
 HAVING COUNT(liked_date) = 0
 
 -- Output: --
-|   page_id |
-|   20701   |
-|   32728   |
+╔═════════╗
+║ page_id ║
+╠═════════╣
+║ 20701   ║
+╠═════════╣
+║ 32728   ║
+╚═════════╝
