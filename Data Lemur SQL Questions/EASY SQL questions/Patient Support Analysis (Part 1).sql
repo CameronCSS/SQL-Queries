af1972a0f5484b9958b071d2db9a76facae2f791
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-04-02 18:09:05 -0600

    Update Patient Support Analysis (Part 1).sql

--- Data Lemur SQL Questions/EASY SQL questions/Patient Support Analysis (Part 1).sql
+++ Data Lemur SQL Questions/EASY SQL questions/Patient Support Analysis (Part 1).sql
@@ -4,13 +4,11 @@
 
                                       -- callers Example Table:  --
                                       
-| policy_holder_id |    case_id         |  call_category  |     call_received   | call_duration_secs| original_order
-    50837000      | dc63-acae-4f39-bb04 |   claims        | 03/09/2022 02:51:00 |       205         |     130
-    50837000      | 41be-bebe-4bd0-a1ba |   IT_support    | 03/12/2022 05:37:00 |       254         |     129
-    50936674      | 12c8-b35c-48a3-b38d |   claims        | 05/31/2022 7:27:00	|       240         |     31
-    50886837      | d0b4-8ea7-4b8c-aa8b |   IT_support    | 03/11/2022 3:38:00	|       276         |     16
-    50886837      | a741-c279-41c0-90ba	|                 | 03/19/2022 10:52:00 |       131         |     325
-    50837000      | bab1-3ec5-4867-90ae |   benefits      | 05/13/2022 18:19:00	|       228         |     339
+| policy_holder_id |       case_id       | call_category |    call_received    | call_duration_secs | original_order |
+|:----------------:|:-------------------:|:-------------:|:-------------------:|:------------------:|:--------------:|
+|     50837000     | dc63-acae-4f39-bb04 |     claims    | 03/09/2022 02:51:00 |         205        |       130      |
+|     50837000     | 41be-bebe-4bd0-a1ba |   IT_support  | 03/12/2022 05:37:00 |         254        |       129      |
+|     50936674     | 12c8-b35c-48a3-b38d |     claims    |  05/31/2022 7:27:00 |         240        |       31       |
     
     
  -- Submission: --
@@ -27,4 +25,5 @@ WHERE members >=3
  
 -- Output: --
 | member_count |
-      76
+|:------------:|
+|      76      |
