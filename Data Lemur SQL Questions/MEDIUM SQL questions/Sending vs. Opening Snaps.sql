1a986d6926ffcef7b10bf850c90200a201999fbf
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-04-03 15:20:33 -0600

    Create Sending vs. Opening Snaps.sql

---
+++ Data Lemur SQL Questions/MEDIUM SQL questions/Sending vs. Opening Snaps.sql
@@ -0,0 +1,48 @@
+-- Assume you are given the tables below containing information on Snapchat users, their ages, 
+    --and their time spent sending and opening snaps. 
+    
+--Write a query to obtain a breakdown of the time spent sending vs. opening snaps (as a percentage of total time spent on these activities) for each age group.
+
+-- Output the age bucket and percentage of sending and opening snaps. Round the percentage to 2 decimal places.
+
+               -- Notes: --
+-- You should calculate these percentages:
+-- time sending / (time sending + time opening)
+-- time opening / (time sending + time opening)
+-- To avoid integer division in percentages, multiply by 100.0 and not 100.
+
+
+-- activities Example Table: --
+| activity_id | user_id | activity_type | time_spent |    activity_date    |
+|:-----------:|:-------:|:-------------:|:----------:|:-------------------:|
+|     7274    |   123   |      open     |    4.50    | 06/22/2022 12:00:00 |
+|     2425    |   123   |      send     |    3.50    | 06/22/2022 12:00:00 |
+|     1413    |   456   |      send     |    5.67    | 06/23/2022 12:00:00 |
+
+
+-- Solution: --
+SELECT age_breakdown.age_bucket, 
+ROUND( SUM(send_time) / (SUM(send_time) + SUM(open_time)) * 100, 2)::numeric AS send_perc, 
+ROUND( SUM(open_time) / (SUM(send_time) + SUM(open_time)) * 100, 2)::numeric AS open_perc
+FROM age_breakdown
+INNER JOIN 
+  (SELECT user_id, SUM(time_spent) AS send_time 
+   FROM activities 
+   WHERE activity_type = 'send' 
+   GROUP BY user_id) AS sending 
+ON age_breakdown.user_id = sending.user_id
+INNER JOIN 
+  (SELECT user_id, SUM(time_spent) AS open_time 
+   FROM activities 
+   WHERE activity_type = 'open' 
+   GROUP BY user_id) AS opening 
+ON age_breakdown.user_id = opening.user_id
+GROUP BY age_breakdown.age_bucket;
+
+
+              -- Output --
+| age_bucket | send_perc | open_perc |
+|:----------:|:---------:|:---------:|
+|    21-25   |   54.31   |   45.69   |
+|    26-30   |   82.26   |   17.74   |
+|    31-35   |   37.84   |   62.16   |
