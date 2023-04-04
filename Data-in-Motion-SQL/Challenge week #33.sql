8702b6750e3685747d927ed368709b42a1186a2e
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-04-03 22:08:10 -0600

    Update and rename Challenge week #33 part 1.sql to Challenge week #33.sql

--- Data-in-Motion-SQL/Challenge week #33.sql
+++ Data-in-Motion-SQL/Challenge week #33.sql
@@ -1,4 +1,7 @@
 -- Data in Motion SQL Challenge week #33
+
+-- QUESTION 1:
+
 -- The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages. The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
 
 -- Write an SQL query to find the confirmation rate of each user.
@@ -42,3 +45,34 @@ SELECT user_id, COALESCE(ROUND((confirmed / actions),2),0) AS confirmation_rate
 | 7       | 1                 |
 | 2       | 0.5               |
 | 6       | 0                 |
+
+
+--------------------------------------
+
+-- QUESTION 2:
+
+-- Write an SQL query that reports the product_name, year, and price for each sale_id in the Sales table.
+
+-- Return the resulting table in any order.
+-- The query result format is in the following example.
+
+
+                 -- Sales table: --                          -- Product table: --
+| sale_id | product_id | year | quantity | price |       | product_id | product_name |
+|:-------:|:----------:|:----:|:--------:|:-----:|       |:----------:|:------------:|
+|    1    |     100    | 2008 |    10    |  5000 |       |     100    |     Nokia    |
+|    2    |     100    | 2009 |    12    |  5000 |       |     200    |     Apple    |
+|    7    |     200    | 2011 |    15    |  9000 |       |     300    |    Samsung   |
+
+
+-- Soultion: --
+SELECT product_name, year, price
+FROM Sales s
+JOIN Product p ON s.product_id = p.product_id
+
+         -- Output: -- 
+| product_name | year | price |
+| ------------ | ---- | ----- |
+| Nokia        | 2009 | 5000  |
+| Nokia        | 2008 | 5000  |
+| Apple        | 2011 | 9000  |
