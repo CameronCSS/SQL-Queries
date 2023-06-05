55266780ca40ad8e22b80eaecb10fed2df11171e
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-06-04 18:27:24 -0600

    Updated Code

--- 8 Weeks of SQL/8 Week SQL Challenge # 1/Code.sql
+++ 8 Weeks of SQL/8 Week SQL Challenge # 1/Code.sql
@@ -1,3 +1,7 @@
+-- Create the tables
+
+-- SALES TABLE
+
 CREATE TABLE sales (
   "customer_id" VARCHAR(1),
   "order_date" DATE,
@@ -23,6 +27,7 @@ VALUES
   ('C', '2021-01-01', '3'),
   ('C', '2021-01-07', '3');
  
+ -- MENU TABLE
 
 CREATE TABLE menu (
   "product_id" INTEGER,
@@ -37,6 +42,7 @@ VALUES
   ('2', 'curry', '15'),
   ('3', 'ramen', '12');
   
+-- MEMBERS TABLE
 
 CREATE TABLE members (
   "customer_id" VARCHAR(1),
@@ -48,15 +54,54 @@ INSERT INTO members
 VALUES
   ('A', '2021-01-07'),
   ('B', '2021-01-09');
+  
+------------ 
+
+1. -- What is the total amount each customer spent at the restaurant? 
+
+-- OLD Code
 
 SELECT customer_id, SUM(price) AS total_spent
   FROM sales
 JOIN menu ON sales.product_id = menu.product_id
   GROUP BY customer_id;
+
+-- UPDATED Code
+
+SELECT 
+	customer_id,
+	SUM(price) AS total_spent
+FROM sales s 
+JOIN menu m 
+ON s.product_id = m.product_id 
+GROUP BY customer_id;
+
+
+
+
+2. -- How many days has each customer visited the restaurant?
+
+-- OLD Code
   
   SELECT customer_id, COUNT(DISTINCT order_date) as days_visited
 FROM sales
 GROUP BY customer_id;
+
+-- UPDATED Code
+
+SELECT 
+	customer_id,
+	COUNT(DISTINCT order_date) AS days_visited
+FROM sales s
+GROUP BY customer_id;
+
+
+
+
+
+3. -- What was the first item from the menu purchased by each customer?
+
+-- OLD Code
 
 SELECT sales.customer_id, menu.product_name, Order_date
 FROM sales
@@ -67,11 +112,76 @@ JOIN (
   GROUP BY customer_id
 ) FirstOrder ON sales.customer_id = FirstOrder.customer_id AND sales.order_date = FirstOrder.first_order_date;
 
+-- UPDATED Code
+
+SELECT 
+    customer_id, 
+    GROUP_CONCAT(product_name, ', ') AS ordered_items,
+    order_date
+FROM (
+    SELECT 
+        s.customer_id, 
+        m.product_name, 
+        s.order_date,
+        ROW_NUMBER() OVER (PARTITION BY s.customer_id, s.order_date, m.product_name ORDER BY s.order_date) AS rn
+    FROM sales s
+    JOIN menu m ON s.product_id = m.product_id
+    WHERE s.order_date IN (
+        SELECT MIN(order_date)
+        FROM sales
+        GROUP BY customer_id
+    )
+) AS subquery
+WHERE rn = 1
+GROUP BY customer_id, order_date
+ORDER BY customer_id, order_date;
+
+
+
+
+4. -- What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+-- OLD Code
+
 SELECT menu.product_name, COUNT(sales.product_id) as number_purchased
 FROM sales
 JOIN menu ON sales.product_id = menu.product_id
 GROUP BY menu.product_name
 ORDER BY number_purchased DESC;
+
+-- UPDATED Code
+
+WITH purchases AS (
+    SELECT
+        m.product_name,
+        COUNT(s.product_id) AS number_purchased
+    FROM sales s
+    JOIN menu m ON s.product_id = m.product_id
+    GROUP BY m.product_name
+),
+most AS (
+    SELECT
+        product_name
+    FROM purchases p
+    WHERE number_purchased = (
+        SELECT MAX(number_purchased)
+        FROM purchases p
+    )
+)
+SELECT 
+    product_name,
+    COUNT(order_date) AS total_orders,
+    customer_id AS customer
+FROM most
+JOIN sales s
+GROUP BY customer_id, product_name
+
+
+
+
+5. -- Which item was the most popular for each customer?
+
+-- OLD Code
 
 WITH purchases AS (
  SELECT sales.customer_id, menu.product_name, COUNT(sales.product_id) as number_purchased,
@@ -84,11 +194,66 @@ SELECT customer_id, product_name, number_purchased
 FROM purchases
 WHERE ranking = 1;
 
+-- UPDATED Code
+
+   WITH purchases AS (
+	  SELECT 
+		  sales.customer_id, 
+		  menu.product_name, 
+		  COUNT(sales.product_id) as number_purchased,
+		  RANK() OVER (PARTITION BY sales.customer_id ORDER BY COUNT(sales.product_id) DESC) AS ranking
+	  FROM sales
+	  JOIN menu ON sales.product_id = menu.product_id
+	  GROUP BY sales.customer_id, menu.product_name
+)
+
+SELECT 
+customer_id, 
+product_name, 
+number_purchased
+FROM purchases
+WHERE ranking = 1;
+
+
+
+
+6. -- Which item was purchased first by the customer after they became a member?
+
+-- OLD Code
+
 SELECT sales.customer_id, menu.product_name, sales.order_date
 FROM sales
 JOIN menu ON sales.product_id = menu.product_id
 JOIN members ON sales.customer_id = members.customer_id
 WHERE sales.order_date > members.join_date;
+
+-- UPDATED Code
+
+SELECT 
+    customer_id,
+    product_name,
+    order_date
+FROM (
+    SELECT 
+        mem.customer_id,
+        m.product_name, 
+        s.order_date,
+        ROW_NUMBER() OVER (PARTITION BY mem.customer_id ORDER BY s.order_date) AS rn
+    FROM members mem
+    JOIN sales s  
+        ON s.customer_id = mem.customer_id
+    JOIN menu m
+        ON m.product_id = s.product_id
+    WHERE s.order_date > mem.join_date
+) AS a
+WHERE rn = 1;
+
+
+
+
+7. -- Which item was purchased just before the customer became a member?
+
+-- OLD Code
 
 SELECT sales.customer_id, menu.product_name, sales.order_date, members.join_date
 FROM sales
@@ -97,12 +262,61 @@ JOIN members ON sales.customer_id = members.customer_id
 WHERE sales.order_date < members.join_date
 ORDER BY sales.customer_id, sales.order_date DESC;
 
+-- UPDATED Code
+
+SELECT 
+    customer_id,
+    product_name,
+    order_date
+FROM (
+    SELECT 
+        mem.customer_id,
+        m.product_name, 
+        s.order_date,
+        ROW_NUMBER() OVER (PARTITION BY mem.customer_id ORDER BY s.order_date desc) AS rn
+    FROM members mem
+    JOIN sales s  
+        ON s.customer_id = mem.customer_id
+    JOIN menu m
+        ON m.product_id = s.product_id
+    WHERE s.order_date < mem.join_date
+) AS a
+WHERE rn = 1;
+
+
+
+
+8. -- What is the total items and amount spent for each member before they became a member?
+
+-- OLD Code
+
 SELECT sales.customer_id, COUNT(sales.product_id) as number_items, SUM(menu.price) as amount_spent
 FROM sales
 JOIN menu ON sales.product_id = menu.product_id
 JOIN members ON sales.customer_id = members.customer_id
 WHERE sales.order_date < members.join_date
 GROUP BY sales.customer_id;
+
+-- UPDATED Code
+
+SELECT 
+	s.customer_id, 
+	COUNT(s.product_id) as num_items, 
+	SUM(m.price) as spent
+FROM sales s
+JOIN menu m
+	ON s.product_id = m.product_id
+JOIN members mem
+	ON s.customer_id = mem.customer_id
+WHERE s.order_date < mem.join_date
+GROUP BY s.customer_id;
+
+
+
+
+9. -- If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+-- OLD Code
 
 SELECT sales.customer_id, 
   SUM((CASE 
@@ -111,6 +325,28 @@ SELECT sales.customer_id,
 FROM sales
 JOIN menu ON sales.product_id = menu.product_id
 GROUP BY sales.customer_id;
+
+-- UPDATED Code
+
+SELECT s.customer_id, 
+  SUM((
+  CASE 
+    WHEN product_name = 'sushi' THEN price * 2 
+	ELSE price END) * 10) AS total_points
+FROM sales s
+JOIN menu m 
+	ON s.product_id = m.product_id
+JOIN members mem
+	ON mem.customer_id = s.customer_id
+GROUP BY s.customer_id HAVING COUNT(join_date) <> 0;
+
+
+
+
+10. -- In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi
+-- how many points do customer A and B have at the end of January?
+
+-- OLD Code
 
 WITH dates AS 
 (
@@ -138,5 +374,34 @@ JOIN dates ON purchases.customer_id = dates.customer_id
 WHERE order_date < dates.last_day
 GROUP BY purchases.customer_id;
 
+-- UPDATED Code
+
+WITH purchases AS (
+	SELECT 
+		s.customer_id,
+		m.product_name,
+ 		m.price,
+    	s.order_date,
+    	mem.join_date,
+    	(
+    		CASE
+    			WHEN product_name = 'sushi' THEN m.price * 2
+    			WHEN s.order_date BETWEEN mem.join_date AND date(mem.join_date, '+6 days') THEN m.price * 2
+    			ELSE m.price
+    		END
+    	) * 10 AS points
+    FROM sales s
+    JOIN menu m 
+    	ON s.product_id = m.product_id
+    JOIN members mem
+    	ON s.customer_id = mem.customer_id
+)
+
+SELECT
+	p.customer_id,
+	SUM(points) AS total_points
+FROM purchases p
+WHERE order_date < (SELECT date(join_date, 'start of month', '+1 month', '-1 day') FROM members)
+GROUP BY p.customer_id;
 
 
