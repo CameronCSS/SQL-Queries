25fac09b9ab45a08e6681f3e1c3a2975a161ea09
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2024-08-14 17:02:02 -0600

    Update User's Third Transaction.sql

--- Data Lemur SQL Questions/MEDIUM SQL questions/User's Third Transaction.sql
+++ Data Lemur SQL Questions/MEDIUM SQL questions/User's Third Transaction.sql
@@ -8,7 +8,9 @@
 | 111     | 55.00  | 01/10/2022 12:00:00 |
 | 121     | 36.00  | 01/18/2022 12:00:00 |
 
--- Solution: --
+
+--  OLD SOLUTION from 1 Year Ago --
+-- OLD Solution: --
 WITH trans_x AS (
 SELECT user_id, MAX(transaction_date) AS transaction_date, COUNT(transaction_date) AS transactions
 FROM transactions
@@ -20,6 +22,44 @@ FROM trans_x t2
 JOIN transactions t1
 ON t1.transaction_date = t2.transaction_date
 
+-- OUTPUT --
+| user_id | spend  | transaction_date    |
+|---------|--------|---------------------|
+| 111     | 89.60  | 02/05/2022 12:00:00 |
+| 121     | 67.90  | 04/03/2022 12:00:00 |
+| 263     | 100.00 | 07/12/2022 12:00:00 |
+
+
+NEW -- NEW SOLUTION !! --
+-- The problem with the old solution was it was overly complex for no real reason. 
+-- It assumed you had to verify someone had at least 3 transactions and then find the 3rd.
+
+-- NEW SOLUTION !! --
+-- We could do this with a subquery instead of a CTE. But I pretty much always prefer to use a CTE --
+
+WITH dates AS (
+SELECT 
+  user_id,
+  spend,
+  transaction_date,
+  ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY transaction_date) AS row
+FROM 
+  transactions
+)
+
+SELECT
+  user_id,
+  spend,
+  transaction_date
+FROM
+  dates
+WHERE
+  row = 3
+
+
+-- NOTES--
+-- WE could order by user id to clean things up, but if you dont HAVE to do it, then dont. you save resources and query time.
+  
 -- OUTPUT --
 | user_id | spend  | transaction_date    |
 |---------|--------|---------------------|
